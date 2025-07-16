985fe311df421a8451397acaf1c76b8b6429ea98
Daplein <rdantoniot28@gmail.com>
2025-07-16 14:52:33 +0300

    modification composer et adminblade

--- resources/views/backOffice/layouts/admin.blade.php
+++ resources/views/backOffice/layouts/admin.blade.php
@@ -6,6 +6,7 @@
     <title>@yield('title', 'backOffice')</title>
     <link href="{{ mix('css/app.css') }}" rel="stylesheet">
     <script src="{{ mix('js/app.js') }}" defer></script>
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" rel="stylesheet" />
 </head>
 
 <body class="bg-gray-100 min-h-screen">
